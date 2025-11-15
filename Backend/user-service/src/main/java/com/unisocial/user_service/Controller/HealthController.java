8fa988066b4a1c0dc03660e43b8d59d9e59a428c
Mohit Chaturvedi <rockmohit54@gmail.com>
2025-11-15 17:33:42 -0500

    Initial Commit: User Service Skeleton

---
+++ Backend/user-service/src/main/java/com/unisocial/user_service/Controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.unisocial.user_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	
+	@GetMapping("/health")
+	public String health(){
+		
+		return "User Service is running, All good here!";
+		
+	}
+
+}
